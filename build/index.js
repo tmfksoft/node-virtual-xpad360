bf37c62eb4dc134f17599964a2aaae3d0a7052ce
Thomas Burnett-Taylor <tmfksoft@gmail.com>
2022-02-06 05:12:20 +0000

    feat: Added first version

---
+++ build/index.js
@@ -0,0 +1,247 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var uinput_1 = __importDefault(require("uinput"));
+var XboxController = /** @class */ (function () {
+    function XboxController() {
+        this.createConfig = {
+            name: 'Xbox360 Controller',
+            absmax: 2048,
+            absmin: -2048,
+            ffEffectsMax: 69,
+            id: {
+                busType: uinput_1.default.events.BUS_USB,
+                vendor: 0x045e,
+                product: 0x028e,
+                version: 0x0110,
+            },
+        };
+        this.setupConfig = {
+            UI_SET_EVBIT: [
+                uinput_1.default.events.EV_KEY,
+                uinput_1.default.events.EV_ABS,
+                uinput_1.default.events.EV_FF,
+            ],
+            // What keys can be pressed.
+            UI_SET_KEYBIT: [
+                uinput_1.default.events.BTN_A,
+                uinput_1.default.events.BTN_B,
+                uinput_1.default.events.BTN_X,
+                uinput_1.default.events.BTN_Y,
+                uinput_1.default.events.BTN_SELECT,
+                uinput_1.default.events.BTN_START,
+                uinput_1.default.events.BTN_THUMBL,
+                uinput_1.default.events.BTN_THUMBR,
+                uinput_1.default.events.BTN_TL,
+                uinput_1.default.events.BTN_TR,
+                uinput_1.default.events.BTN_MODE,
+            ],
+            // Enables Force Feedback, though we don't support it currently.
+            UI_SET_FFBIT: [
+                uinput_1.default.events.FF_PERIODIC,
+                uinput_1.default.events.FF_RUMBLE,
+                uinput_1.default.events.FF_GAIN,
+                uinput_1.default.events.FF_SQUARE,
+                uinput_1.default.events.FF_TRIANGLE,
+                uinput_1.default.events.FF_SINE,
+            ],
+            UI_SET_ABSBIT: [
+                uinput_1.default.events.ABS_X,
+                uinput_1.default.events.ABS_Y,
+                uinput_1.default.events.ABS_RX,
+                uinput_1.default.events.ABS_RY,
+                uinput_1.default.events.ABS_HAT0X,
+                uinput_1.default.events.ABS_HAT0Y,
+                uinput_1.default.events.ABS_Z,
+                uinput_1.default.events.ABS_RZ, // Right Trigger
+            ],
+        };
+    }
+    /**
+     * Creates a UInput device or returns the current one.
+     * @returns
+     */
+    XboxController.prototype.createDevice = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var device;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (this.device) {
+                            return [2 /*return*/, this.device];
+                        }
+                        return [4 /*yield*/, uinput_1.default.setup(this.setupConfig)];
+                    case 1:
+                        device = _a.sent();
+                        return [4 /*yield*/, device.create(this.createConfig)];
+                    case 2:
+                        _a.sent();
+                        this.device = device;
+                        return [2 /*return*/, device];
+                }
+            });
+        });
+    };
+    /**
+     * Creates the device
+     */
+    XboxController.prototype.setup = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.createDevice()];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    /**
+     * Destroys and cleans up the device.
+     */
+    XboxController.prototype.destroy = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                if (this.device) {
+                    this.device.destroy();
+                }
+                return [2 /*return*/];
+            });
+        });
+    };
+    /**
+     * Presses a button down
+     * @param button Button to press
+     * @returns void
+     */
+    XboxController.prototype.buttonDown = function (button) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!this.device) {
+                            return [2 /*return*/];
+                        }
+                        return [4 /*yield*/, this.device.keyEvent(button, true)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    /**
+     * Releases a pressed button
+     * @param button Button to release
+     * @returns void
+     */
+    XboxController.prototype.buttonUp = function (button) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!this.device) {
+                            return [2 /*return*/];
+                        }
+                        return [4 /*yield*/, this.device.keyEvent(button, false)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    //32767
+    /**
+     * Sets the value of an axis.
+     * @param axis Axis to modify
+     * @param value Value from -1 to 1 (Gets mapped accordingly)
+     * @returns void
+     */
+    XboxController.prototype.setAxis = function (axis, value) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!this.device) {
+                            return [2 /*return*/];
+                        }
+                        return [4 /*yield*/, this.device.sendEvent(uinput_1.default.events.EV_ABS, axis, this.map(value, -1, 1, -32767, 32767))];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    // See https://gist.github.com/xposedbones/75ebaef3c10060a3ee3b246166caab56
+    XboxController.prototype.clamp = function (input, min, max) {
+        return input < min ? min : input > max ? max : input;
+    };
+    XboxController.prototype.map = function (current, in_min, in_max, out_min, out_max) {
+        var mapped = ((current - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+        return this.clamp(mapped, out_min, out_max);
+    };
+    /**
+     * Simple delay helper, wraps setTimeout in a promise, useful to halt your script for a set amount of time.
+     * @param time Time to wait in ms
+     * @returns
+     */
+    XboxController.prototype.sleep = function (time) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, new Promise(function (resolve) { return setTimeout(resolve, time); })];
+            });
+        });
+    };
+    return XboxController;
+}());
+var controller = new XboxController();
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            controller.setup();
+            return [2 /*return*/];
+        });
+    });
+}
+main();
---
+++ src/index.ts
@@ -0,0 +1,161 @@
+import UInput from 'uinput';
+
+class XboxController {
+
+	public createConfig = {
+		name: 'Xbox360 Controller',
+		absmax: 2048,
+		absmin: -2048,
+		ffEffectsMax: 69,
+		id: {
+			busType: UInput.events.BUS_USB,
+			vendor: 0x045e,
+			product: 0x028e,
+			version: 0x0110,
+		},
+	};
+
+	public setupConfig = {
+		UI_SET_EVBIT:  [
+			UInput.events.EV_KEY,
+			UInput.events.EV_ABS,
+			UInput.events.EV_FF,
+		],
+		// What keys can be pressed.
+		UI_SET_KEYBIT: [
+			UInput.events.BTN_A,
+			UInput.events.BTN_B,
+			UInput.events.BTN_X,
+			UInput.events.BTN_Y,
+			UInput.events.BTN_SELECT, // Start
+			UInput.events.BTN_START, // Back
+	
+			UInput.events.BTN_THUMBL,
+			UInput.events.BTN_THUMBR,
+	
+			UInput.events.BTN_TL,
+			UInput.events.BTN_TR,
+			UInput.events.BTN_MODE,
+		],
+	
+		// Enables Force Feedback, though we don't support it currently.
+		UI_SET_FFBIT: [
+			UInput.events.FF_PERIODIC,
+			UInput.events.FF_RUMBLE,
+			UInput.events.FF_GAIN,
+			UInput.events.FF_SQUARE,
+			UInput.events.FF_TRIANGLE,
+			UInput.events.FF_SINE,
+		],
+		UI_SET_ABSBIT: [
+			UInput.events.ABS_X, // Left Stick X
+			UInput.events.ABS_Y, // Left Stick Y
+			UInput.events.ABS_RX, // Right Stick X
+			UInput.events.ABS_RY, // Right Stick Y
+	
+			UInput.events.ABS_HAT0X, // Dpad
+			UInput.events.ABS_HAT0Y, // Dpad
+	
+			UInput.events.ABS_Z, // Left Trigger
+			UInput.events.ABS_RZ, // Right Trigger
+		],
+	};
+	
+	public device: UInput.UInput | undefined;
+
+	/**
+	 * Creates a UInput device or returns the current one.
+	 * @returns 
+	 */
+    async createDevice(): Promise<UInput.UInput> {
+        if (this.device) {
+            return this.device;
+        }
+
+        const device = await UInput.setup(this.setupConfig);
+        await device.create(this.createConfig);
+
+		this.device = device;
+
+        return device;
+    }
+
+	/**
+	 * Creates the device
+	 */
+	async setup() {
+		await this.createDevice();
+	}
+
+	/**
+	 * Destroys and cleans up the device.
+	 */
+	async destroy() {
+		if (this.device) {
+			this.device.destroy();
+		}
+	}
+
+	/**
+	 * Presses a button down
+	 * @param button Button to press
+	 * @returns void
+	 */
+	async buttonDown(button: number) {
+		if (!this.device) {
+			return;
+		}
+		await this.device.keyEvent(button, true);
+	}
+	/**
+	 * Releases a pressed button
+	 * @param button Button to release
+	 * @returns void
+	 */
+	async buttonUp(button: number) {
+		if (!this.device) {
+			return;
+		}
+		await this.device.keyEvent(button, false);
+	}
+	//32767
+
+	/**
+	 * Sets the value of an axis.
+	 * @param axis Axis to modify
+	 * @param value Value from -1 to 1 (Gets mapped accordingly)
+	 * @returns void
+	 */
+	async setAxis(axis: number, value: number) {
+		if (!this.device) {
+			return;
+		}
+		await this.device.sendEvent(UInput.events.EV_ABS, axis, this.map(value, -1, 1, -32767, 32767));
+	}
+
+	// See https://gist.github.com/xposedbones/75ebaef3c10060a3ee3b246166caab56
+	clamp(input: number, min: number, max: number): number {
+		return input < min ? min : input > max ? max : input;
+	}
+	
+	map(current: number, in_min: number, in_max: number, out_min: number, out_max: number): number {
+		const mapped: number = ((current - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+		return this.clamp(mapped, out_min, out_max);
+	}
+
+	/**
+	 * Simple delay helper, wraps setTimeout in a promise, useful to halt your script for a set amount of time.
+	 * @param time Time to wait in ms
+	 * @returns 
+	 */
+	async sleep (time: number) {
+		return new Promise((resolve) => setTimeout(resolve, time));
+	}
+}
+
+const controller = new XboxController();
+async function main() {
+	controller.setup();
+
+}
+main();
